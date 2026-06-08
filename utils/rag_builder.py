fa9ffe640ee1384fb2fa3903d80060226f46b08d
Mansijaysingh <mansisingh.am000@gmail.com>
2026-06-08 11:51:27 +0530

    Create RAG builder with chunking and FAISS retriever

---
+++ utils/rag_builder.py
@@ -0,0 +1,34 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+
+
+def build_vector_store(texts_dict):
+
+  text_splitter=RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=50
+  )
+
+  documents=[]
+
+  for doc_name, text in texts_dict.items():
+
+    chunks=text_splitter.create_documents(
+      [text],
+      metadatas=[{"source": doc_name}]
+    )
+
+    documents.extend(chunks)
+
+
+  embeddings=OpenAIEmbeddings(
+    model="text-embedding-3-small"
+  )
+
+  vectore_store=FAISS.from_documents(
+    documents,
+    embeddings
+  )
+
+  return vectore_store.as_retriever()
